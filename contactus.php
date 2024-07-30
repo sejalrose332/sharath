fea979ac1e9e4e621e46c226d4a28fd1efa6a63c
unknown <habelash@gmail.com>
2024-07-30 14:59:24 +0530

    Changed

--- contactus.php
+++ contactus.php
@@ -186,7 +186,7 @@
                                     $dbservername = 'localhost';
                                     $dbusername = 'root';
                                     $dbpassword = '';
-                                    $dbname = 'users';
+                                    $dbname = 'enquiry';
                                     $conn = mysqli_connect($dbservername,$dbusername,$dbpassword,$dbname);
 
                                     $YourName=$_GET['Your Name'];
